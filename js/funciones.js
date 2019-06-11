e607e4eece94deda442343cb3daed4c21a8abcc2
LuceroJose <jose.maximiliano.lucero@gmail.com>
2019-06-11 15:42:14 -0300

    empezando con la lista

--- js/funciones.js
+++ js/funciones.js
@@ -1,13 +1,28 @@
-let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-scanner.addListener('scan', function(content) {
-    console.log(content);
+
+//CODIGO DEL SCANNER 
+$( document ).ready(function(){
+    let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    scanner.addListener('scan', function(content) {
+        window.location=content;
+        sessionStorage.setItem(content,"visitado");
+    });
+    Instascan.Camera.getCameras().then(function(cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No se encontro ninguna camara en el dispositivo!.');
+        }
+    }).catch(function(e) {
+        console.error(e);
+    });
 });
-Instascan.Camera.getCameras().then(function(cameras) {
-    if (cameras.length > 0) {
-        scanner.start(cameras[0]);
-    } else {
-        console.error('No cameras found.');
+/******************************/
+
+
+//Lista de visitados
+function listado(){
+    for(var i=0; i<sessionStorage.length; i++){
+        var nombre = sessionStorage.key(i);
+        console.log(nombre);
     }
-}).catch(function(e) {
-    console.error(e);
-});
+}
